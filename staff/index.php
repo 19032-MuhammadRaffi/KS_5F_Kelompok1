bcf8922fb2b7eeeb9f23db8164a8bb18ba22d0c8
19117-RaflyDanuRizki <83756351+19117-RaflyDanuRizki@users.noreply.github.com>
2021-12-15 10:39:59 +0700

    Update index.php

--- staff/index.php
+++ staff/index.php
@@ -1,3 +1,6 @@
+<?php
+    require 'function/F_index.php';
+?>
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -8,35 +11,39 @@
 <body>
     <div class="d-flex" id="wrapper">
         <?php include 'sidebar.php'; ?>
-<!-- Status Box -->
+        <div id="page-content-wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light py-4 px-4">
+                <div class="d-flex align-items-center">
+                    <i class="fas fa-align-left fs-4 me-3" id="menu-toggle"></i>
+                    <h2 class="fs-2 m-0">Menu</h2>
+                </div>
+            </nav>
             <div class="container-fluid px-4">
-            <div class="row g-3 my-2">
+<!-- Status Box -->
+                <div class="row g-3 my-2">
                     <div class="col-md-4">
                         <div class="p-3 bg-4 shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2 warna-2 pt-2"><?php echo "10" ?></h3>
-                                <p class="fs-5 fw-bold warna-2" name>Materi</p>
+                                <h3 class="fs-2 warna-2 pt-2"><?php echo $rowData ?></h3>
+                                <p class="fs-5 fw-bold warna-2" name>Data Laptop</p>
                             </div>
                             <i class="fas fa-book-reader fs-1 warna-3 rounded-full bg-light p-3"></i>
                         </div>
                     </div>
-
                     <div class="col-md-4">
                         <div class="p-3 bg-4 shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2 warna-2 pt-2"><?php echo "10" ?></h3>
-                                <p class="fs-5 fw-bold warna-2">Tugas</p>
+                                <h3 class="fs-2 warna-2 pt-2"><?php echo $rowSeries ?></h3>
+                                <p class="fs-5 fw-bold warna-2">Series Laptop</p>
                             </div>
-                            <i
-                                class="fas fa-tasks fs-1 warna-3 rounded-full bg-light p-3"></i>
+                            <i class="fas fa-tasks fs-1 warna-3 rounded-full bg-light p-3"></i>
                         </div>
                     </div>
-
                     <div class="col-md-4">
                         <div class="p-3 bg-4 shadow-sm d-flex justify-content-around align-items-center rounded">
                             <div>
-                                <h3 class="fs-2 warna-2 pt-2"><?php echo "10" ?></h3>
-                                <p class="fs-5 fw-bold warna-2">Siswa</p>
+                                <h3 class="fs-2 warna-2 pt-2"><?php echo $rowStok[0] ?></h3>
+                                <p class="fs-5 fw-bold warna-2">Jumlah Stok</p>
                             </div>
                             <i class="fas fa-users fs-1 warna-3 rounded-full bg-light p-3"></i>
                         </div>
@@ -44,7 +51,7 @@
                 </div>
 <!-- Page Content -->
                 <div class="row my-5">
-                    <h3 class="fs-4 mb-3">Selamat Datang <?php echo "Nama" ?></h3>
+                    <h3 class="fs-4 mb-3">Selamat Datang <?php echo $rowSession['nama'] ?></h3>
                 </div>
             </div>  
         </div>
