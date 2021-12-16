edf931875be489cdf20adcad1dc9dd99035e190d
19118-RamadhanChandraditio <83756131+19118-RamadhanChandraditio@users.noreply.github.com>
2021-12-16 19:29:10 +0700

    Add files via upload

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -22,10 +22,3 @@
                 onclick="return confirm('Keluar ?')"><i class="bi bi-door-open me-2"></i>Keluar</a>
             </div>
         </div>
-        <div id="page-content-wrapper">
-            <nav class="navbar navbar-expand-lg navbar-light py-4 px-4">
-                <div class="d-flex align-items-center">
-                    <i class="fas fa-align-left fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-2 m-0">Menu</h2>
-                </div>
-            </nav>
