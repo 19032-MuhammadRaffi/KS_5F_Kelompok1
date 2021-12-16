97f847afcd03ca5420f97cdd7308a6aa64c24ee4
Muhammad Raffi <83756077+19032-MuhammadRaffi@users.noreply.github.com>
2021-12-16 18:55:29 +0700

    Final - Back End Data Laptop

--- staff/function/F_data_laptop.php
+++ staff/function/F_data_laptop.php
@@ -63,7 +63,104 @@
         }
     }
 //Edit Data Laptop
+    if(isset($_POST["editData"])){
+        $id_laptop = $_POST["id_laptop"];
+        $series = $_POST["series"];
+        $nama_laptop = $_POST["nama_laptop"];
+        $merk = $_POST["merk"];
+        $processor = $_POST["processor"];
+        $type_storage = $_POST["type_storage"];
+        $size_storage = $_POST["size_storage"];
+        $vga_discrete = $_POST["vga_discrete"];
+        $ram = $_POST["ram"];
+        $camera = $_POST["camera"];
+        $dvd = $_POST["dvd"];
+        $harga = $_POST["harga"];
+        // File Upload
+        $namaAsli = $_FILES['gambar_laptop']['name'];
+        if($namaAsli==""){
+            $queryUpdate = "UPDATE data_laptop SET series='$series', nama_laptop='$nama_laptop', merk='$merk',
+             processor='$processor', type_storage='$type_storage', size_storage='$size_storage', vga_discrete='$vga_discrete', 
+             ram='$ram', camera='$camera', dvd='$dvd', harga='$harga' WHERE id_laptop='$id_laptop'";
+            $executeUpdate = mysqli_query($koneksi, $queryUpdate) or die($koneksi);
+            if ($executeUpdate){
+                echo "
+                    <script>
+                        alert('Berhasil Update Data Laptop $id_laptop !');
+                        document.location.href = 'data_laptop.php';
+                    </script>
+                ";
+            }
+        }
+        else{
+            // Hapus Gambar Lama
+            $cariFile = mysqli_query($koneksi, "SELECT * FROM data_laptop WHERE id_laptop = '$id_laptop'") or die(mysqli_error($koneksi));
+            $cariRow = mysqli_fetch_array($cariFile);
+            $namaFile = $cariRow['gambar_laptop'];
+            $lokasi = "../gambarLaptop/".$namaFile;
+            if (file_exists($lokasi)){
+                unlink($lokasi);
+            }
+            $x = explode('.',$namaAsli);
+            $eks = strtolower(end($x));
+            $asal = $_FILES['gambar_laptop']['tmp_name'];
+            $dir = "../gambarLaptop/";
+            $gambar_laptop = uniqid();
+            $gambar_laptop .= '.'.$eks;
+            $targetFile = $dir.$gambar_laptop;
+            $uploadOk = 1;
+            // Cek apakah file sudah ada difolder ?
+            if (file_exists($targetFile)){
+                $uploadOk = 0;
+            }
+            // Cek Proses Upload
+            if ($uploadOk == 0){
+                echo '<script>alert("Nama file sudah ada!");</script>';
+            } 
+            else {
+                if (move_uploaded_file($asal, $targetFile)){
+                    $queryUpdate = "UPDATE data_laptop SET series='$series', nama_laptop='$nama_laptop', merk='$merk',
+                    processor='$processor', type_storage='$type_storage', size_storage='$size_storage', vga_discrete='$vga_discrete', 
+                    ram='$ram', camera='$camera', dvd='$dvd', harga='$harga', gambar_laptop='$gambar_laptop' WHERE id_laptop='$id_laptop'";
+                    $executeUpdate = mysqli_query($koneksi, $queryUpdate) or die($koneksi);
+                    if ($executeUpdate){
+                        echo "<script>alert('Data Laptop $id_laptop berhasil diedit !')
+                        window.location.replace('data_laptop.php');</script>";
+                    }
+                } else {
+                    echo '<script>alert("Proses Upload GAGAL!");</script>';
+                }
+            }
+        }
+    }
 //Delete Data Laptop
+    if(isset($_POST["hapusData"])){
+        $getID = $_POST['getID'];
+        $cariFile = mysqli_query($koneksi, "SELECT * FROM data_laptop WHERE id_laptop = '$getID'") or die(mysqli_error($koneksi));
+        $cariRow = mysqli_fetch_array($cariFile);
+        $namaFile = $cariRow['gambar_laptop'];
+        $lokasi = "../gambarLaptop/".$namaFile;
+        if (file_exists($lokasi)){
+            unlink($lokasi);
+        }
+        $queryHapus = mysqli_query($koneksi, "DELETE FROM data_laptop WHERE id_laptop = '$getID'") or die($koneksi);
+        if ($queryHapus){
+            echo "
+                <script>
+                    alert('Berhasil Menghapus Data!');
+                    document.location.href = 'data_laptop.php';
+                </script>
+            ";
+        }
+        else{
+            echo "
+                <script>
+                    alert('Gagal Menghapus Data!');
+                    document.location.href = 'data_laptop.php';
+                </script>
+            ";
+        }
+    }
 //Pencarian Data
     if(isset($_POST["searchData"])){
         $cari = $_POST["pencarian"];
