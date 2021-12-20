f5ace86dbdf2f759f4e7443cb6556c4b9532b555
19118-RamadhanChandraditio <83756131+19118-RamadhanChandraditio@users.noreply.github.com>
2021-12-20 13:49:45 +0700

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -70,3 +70,21 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="alertEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Edit data ID User <?php echo $rowSet['id_user'] ?>?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                        <button type="submit" class="btn btn-primary" name="eksekusiEdit">Yes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
