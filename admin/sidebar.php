bfdab2c3c7fb504d0bd0e64ac11f7015dd1e6a75
19118-RamadhanChandraditio <83756131+19118-RamadhanChandraditio@users.noreply.github.com>
2021-12-20 13:47:20 +0700

    Update sidebar.php

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -22,3 +22,51 @@
                 onclick="return confirm('Keluar ?')"><i class="bi bi-door-open me-2"></i>Keluar</a>
             </div>
         </div>
+        <form method="POST">
+            <div class="modal fade" id="edit<?php echo $id_user ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Pengaturan User</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="invisible position-absolute">
+                                <input type="text" class="form-control" name="id_user" value="<?php echo $rowSet['id_user'] ?>">
+                            </div>
+                            <div class="row">
+                                <div class="col-5 mt-1"><label>Nama</label></div>
+                                <div class=col>
+                                <input class="form-control" name="nama" type="text" value="<?php echo $rowSet['nama'] ?>"><br>
+                            </div>
+                            </div>
+                        <div class="row">
+                            <div class="col-5 mt-1"><label>No HP</label></div>
+                            <div class=col>
+                                <input class="form-control" name="no_hp" type="text" value="<?php echo $rowSet['no_hp'] ?>"><br>
+                            </div>
+                            </div>
+                        <div class="row mt-4">
+                            <div class="col-5 mt-1"><label>Password Lama</label></div>
+                            <div class=col>
+                                <input class="form-control" name="password_lama" type="password" placeholder="Masukan Password"><br>
+                            </div>
+                        </div>
+                        <div class="row mt-4">
+                            <div class="col-5 mt-1"><label>Password Baru</label></div>
+                            <div class=col>
+                                <input class="form-control" name="password_baru" type="password" placeholder="Opsional"><br>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <div class="row mt-3">  
+                                <div class="col-md-12 d-flex justify-content-end">
+                                    <button type="button" class="btn btn-secondary mx-2" data-bs-dismiss="modal">Keluar</button>
+                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#alertEditModal">Simpan</button>
+                                </div>                                     
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
