29ac9ac3de05cdd73801214bb6267975d6a88ee5
19111-NurAzizahRetno <83756927+19111-NurAzizahRetno@users.noreply.github.com>
2021-12-20 20:36:37 +0700

    Update_M_Laptop.php

--- staff/M_Laptop.php
+++ staff/M_Laptop.php
@@ -314,3 +314,23 @@
             </div>
         </div>
     </form>
+<!-- Tombol Konfirmasi Hapus -->
+    <form method="POST" class="form-group" enctype="multipart/form-data">
+        <div class="modal fade text-start" id="hapus<?php echo $id_laptop[$i] ?>" tabindex="-1" aria-labelledby="alertHapusModal" aria-hidden="true">
+            <div class="modal-dialog modal-sm modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Pemberitahuan</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Hapus data <?php echo $id_laptop[$i] ?> ?<br>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                        <button name="hapusData" type="submit" class="btn btn-primary">Yes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
