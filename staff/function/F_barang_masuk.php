9e30c4fbc8098aa4d5e01e0d889d5050a703238d
Riandika <84719581+riandikast@users.noreply.github.com>
2021-12-21 07:12:54 +0700

    Update F_barang_masuk.php

--- staff/function/F_barang_masuk.php
+++ staff/function/F_barang_masuk.php
@@ -18,3 +18,19 @@
         $getRow['nama_laptop'] = "";
         $getRow['series'] = "";
     }
+    if(isset($_POST["prosesMasuk"])){
+        $queryID = mysqli_query($koneksi, "SELECT max(id_masuk) as id_terbesar FROM barang_masuk");
+        $data = mysqli_fetch_array($queryID);
+        $id_masuk = $data['id_terbesar'];
+        $urutan = (int) substr($id_masuk, 3, 7);
+        $urutan++;
+        $huruf = "MSK";
+        $id_masuk = $huruf . sprintf("%07s", $urutan);
+        $id_laptop = $_POST['id_laptop'];
+        $id_user = $_SESSION['id_user'];
+        $nama_user = $_SESSION['nama'];
+        $jumlah = $_POST['jumlah'];
+        $queryCreate = "INSERT INTO barang_masuk VALUES ('$id_masuk','$id_user','$id_laptop',NOW(),'$nama_user','$jumlah','Masuk')";
+        $queryUpdate = "UPDATE data_laptop SET stok=stok+$jumlah WHERE id_laptop='$id_laptop'";
+        $createData = mysqli_query($koneksi, $queryCreate);
+        $updateData = mysqli_query($koneksi, $queryUpdate);
