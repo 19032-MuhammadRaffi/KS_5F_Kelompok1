4c2d40057fa58817c6363abdad7d4d63d936f373
Riandika <84719581+riandikast@users.noreply.github.com>
2021-12-21 07:08:32 +0700

    Update F_barang_masuk.php

--- staff/function/F_barang_masuk.php
+++ staff/function/F_barang_masuk.php
@@ -6,3 +6,11 @@
     $queryRead = mysqli_query($koneksi, "SELECT id_laptop, series, nama_laptop, stok FROM data_laptop");
 //Read ID_Laptop
     $queryID = mysqli_query($koneksi, "SELECT id_laptop FROM data_laptop");
+//Transaksi
+    $nowID = "";    
+    if(isset($_POST["pilihID"])){
+        $id_laptop = $_POST['id_laptop'];
+        $nowID = $id_laptop;
+        $queryGetID = mysqli_query($koneksi, "SELECT nama_laptop, series FROM data_laptop WHERE id_laptop='$id_laptop'");
+        $getRow = mysqli_fetch_array($queryGetID);
+    }
