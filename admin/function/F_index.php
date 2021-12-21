ee8b22afea1fecbb2cef099f74ef06129c5951ce
19116-RafifMahfuzhin <84438308+19116-RafifMahfuzhin@users.noreply.github.com>
2021-12-21 11:15:56 +0700

    Update F_index.php

--- admin/function/F_index.php
+++ admin/function/F_index.php
@@ -1,16 +1,16 @@
 <?php
 require '../koneksi.php';
 require 'session.php';
-// Ambil Jumlah Data Laptop
+// Mengambil Jumlah Data Laptop
     $queryData = mysqli_query($koneksi, "SELECT * FROM data_laptop");
     $rowData = mysqli_num_rows($queryData);
-// Ambil Jumlah Series Laptop
+// Mengambil Jumlah Series Laptop
     $querySeries = mysqli_query($koneksi, "SELECT DISTINCT series FROM data_laptop");
     $rowSeries = mysqli_num_rows($querySeries);
-// Ambil Jumlah Stok di Gudang
+// Mengambil Jumlah Stok di Gudang
     $queryStok = mysqli_query($koneksi, "SELECT SUM(stok) FROM data_laptop");
     $rowStok = mysqli_fetch_array($queryStok);
-// Ambil Jumlah Data Pengguna
+// Mengambil Jumlah Data Pengguna
     $queryPengguna = mysqli_query($koneksi, "SELECT * FROM user");
     $rowPengguna = mysqli_num_rows($queryPengguna);
 ?>
