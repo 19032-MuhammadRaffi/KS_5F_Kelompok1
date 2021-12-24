a53dfdb90a8ef15f1025ee989b613f49b0c52a12
19162AnnasFajriansyah <95581674+19162AnnasFajriansyah@users.noreply.github.com>
2021-12-24 20:25:38 +0700

    Update F_index.php

--- admin/function/F_index.php
+++ admin/function/F_index.php
@@ -1,16 +1,16 @@
 <?php
-require '../koneksi.php';
-require 'session.php';
-// Mengambil Jumlah Data Laptop
+    require '../koneksi.php';
+    require 'session.php';
+// Ambil Jumlah Data Laptop
     $queryData = mysqli_query($koneksi, "SELECT * FROM data_laptop");
     $rowData = mysqli_num_rows($queryData);
-// Mengambil Jumlah Series Laptop
+// Ambil Jumlah Series Laptop
     $querySeries = mysqli_query($koneksi, "SELECT DISTINCT series FROM data_laptop");
     $rowSeries = mysqli_num_rows($querySeries);
-// Mengambil Jumlah Stok di Gudang
+// Ambil Jumlah Stok di Gudang
     $queryStok = mysqli_query($koneksi, "SELECT SUM(stok) FROM data_laptop");
     $rowStok = mysqli_fetch_array($queryStok);
-// Mengambil Jumlah Data Pengguna
+// Ambil Jumlah Data Pengguna
     $queryPengguna = mysqli_query($koneksi, "SELECT * FROM user");
     $rowPengguna = mysqli_num_rows($queryPengguna);
 ?>
